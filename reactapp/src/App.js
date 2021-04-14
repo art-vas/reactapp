400abb3efcf70814c964d12ddcd1acfe5f117de6
Artem Vasiukevich <vezuha96@gmail.com>
2021-04-14 22:31:47 +0300

    styles

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,9 +2,15 @@ import React from 'react'
 import TodoList from './Todo/TodoList'
 
 function App() {
+  const todos =[
+    {id: 1, complited: false, title: 'Купить хлеб'},
+    {id: 2, complited: false, title: 'Купить масло'},
+    {id: 3, complited: false, title: 'Купить молоко'},
+  ]
+
   return <div className='wrapper'>
   <h1>React tutuorial</h1>
-  <TodoList></TodoList>
+  <TodoList />
 </div>
   
 }
